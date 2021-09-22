6cf7f23e9a4251cdeebb3b98a6ffbc404db3e3ab
prashanth-hn <hnpb73@gmail.com>
2021-09-22 13:35:06 +0100

    Committing code comments as suggested..

--- TestGitProject01/src/test/java/GitExcersises/HelloGit.java
+++ TestGitProject01/src/test/java/GitExcersises/HelloGit.java
@@ -5,8 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// adding comments for GIT demo.
 		System.out.println("Hello Git... my first GIT push...");
-
 	}
-
 }
